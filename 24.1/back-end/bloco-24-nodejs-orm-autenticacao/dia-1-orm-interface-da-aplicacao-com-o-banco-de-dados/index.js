fff85ca2c5b5459c448e46399d56969eaeba1132
Armando - PC principal Ubuntu <cosilk@hotmail.com>
2022-07-08 20:20:47 -0300

    feat: atualiza

--- 24.1/back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.json());
-
-app.listen(PORT, () => console.log(`Ouvindo a porta ${PORT}!`));
