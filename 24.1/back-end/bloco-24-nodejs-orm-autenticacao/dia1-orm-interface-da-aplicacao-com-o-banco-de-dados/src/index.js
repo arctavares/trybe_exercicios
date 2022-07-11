681ecd428585a248244fae2660b8d095d0d2be23
Armando - PC principal Ubuntu <cosilk@hotmail.com>
2022-07-11 18:20:28 -0300

    exercicios 3,4,5

--- 24.1/back-end/bloco-24-nodejs-orm-autenticacao/dia1-orm-interface-da-aplicacao-com-o-banco-de-dados/src/index.js
+++ 24.1/back-end/bloco-24-nodejs-orm-autenticacao/dia1-orm-interface-da-aplicacao-com-o-banco-de-dados/src/index.js
@@ -1,7 +1,10 @@
+const BooksController = require('./controllers/BooksController');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.get('/books', BooksController.getAll);
 
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
